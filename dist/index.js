2e50af1bd7c9f280ab9ce132c9f3f4af35561565
mcbee <mcbee@mcbees-Mac-mini.local>
2026-02-05 15:18:00 -0500

    feat: add test infrastructure (items 1-2)

---
+++ dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.main = main;
+function main() {
+    console.log("PRD Generator CLI");
+}
+if (require.main === module) {
+    main();
+}
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export function main(): void {
+  console.log("PRD Generator CLI");
+}
+
+if (require.main === module) {
+  main();
+}
